d14cd23ae273dd2ee17e38b79986883f8f67223a
Cha Hwa Young <chahwayoung214@gmail.com>
2025-09-08 00:55:06 +0900

    Chore :memo: 코드 포매팅 및 린터 적용

--- okpos_backend/config/urls.py
+++ okpos_backend/config/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, re_path
 
 from django.conf import settings
-from django.conf.urls.static import static
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -10,21 +9,32 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title='OKPOS API',
-        default_version='v1',
-        description='OKPOS Backend Assignment API',
+        title="OKPOS API",
+        default_version="v1",
+        description="OKPOS Backend Assignment API",
         contact=openapi.Contact(email="chahwayoung214@gmail.com"),
     ),
     public=True,
-    permission_classes=[permissions.AllowAny]
+    permission_classes=[permissions.AllowAny],
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
 
 if settings.DEBUG:
     urlpatterns += [
-        re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name="schema-json"),
-        re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-        re_path('doc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')]
+        re_path(
+            r"^swagger(?P<format>\.json|\.yaml)$",
+            schema_view.without_ui(cache_timeout=0),
+            name="schema-json",
+        ),
+        re_path(
+            r"^swagger/$",
+            schema_view.with_ui("swagger", cache_timeout=0),
+            name="schema-swagger-ui",
+        ),
+        re_path(
+            "doc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+        ),
+    ]
