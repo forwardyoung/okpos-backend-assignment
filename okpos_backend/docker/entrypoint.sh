e3a498de86e02fd404cbec02b3b47bcd1fb77538
Cha Hwa Young <chahwayoung214@gmail.com>
2025-09-08 01:30:11 +0900

    Refactor :recycle: Dockerfile - entrypoint.sh 파일로 분리하여 처리

---
+++ okpos_backend/docker/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+echo "Waiting for PostgreSQL to be ready..."
+sleep 10
+
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Starting Gunicorn..."
+exec gunicorn config.wsgi:application --bind 0.0.0.0:8000
